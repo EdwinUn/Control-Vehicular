bfa84ee246a6ad7deac0e454591322d5bd09759f
Edwin <edwingeovanni516@gmail.com>
2026-02-11 17:04:22 -0600

    cambio para pantallas de alta resolución

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-
-"""
-TODO
-
-"""
-
 import sys
 from PySide6.QtWidgets import QApplication
+from PySide6.QtCore import Qt # Importación necesaria para el escalado
 from interfaz import VentanaPrincipal
+
+# Optimización para pantallas de alta resolución
+QApplication.setAttribute(Qt.AA_EnableHighDpiScaling, True)
+QApplication.setAttribute(Qt.AA_UseHighDpiPixmaps, True)
 
 app = QApplication(sys.argv)
 ventana = VentanaPrincipal()
